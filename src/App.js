87b8a25d2e6bb6c8d15e6735f98b708115c348aa
Jarosław Mokrzycki <jmokrzycki@gwo.pl>
2021-12-16 11:19:17 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import interact from 'interactjs';
+
+interact('.item')
+  .draggable({
+    onmove: function(event) {
+      const target = event.target;
+
+      const dataX = target.getAttribute('data-x');
+      const dataY = target.getAttribute('data-y');
+      
+      console.log(dataX, dataY);
+
+      const initialX = parseFloat(dataX) || 0;
+      const initialY = parseFloat(dataY) || 0;
+
+      const deltaX = event.dx;
+      const deltaY = event.dy;
+
+      const newX = initialX + deltaX;
+      const newY = initialY + deltaY;
+
+      target
+        .style
+        .transform = `translate(${newX}px, ${newY}px)`;
+
+      target.setAttribute('data-x', newX);
+      target.setAttribute('data-y', newY);
+    },
+  });
+
+  interact('.dropzone')
+  .dropzone({
+    accept: '.item',
+    overlap: 0.75,
+    ondropactivate: function (event) {
+      const item = event.relatedTarget
+      item.classList.add('dragging')
+    },
+    ondropdeactivate: function (event) {
+      const item = event.relatedTarget
+      item.classList.remove('dragging', 'cannot-drop')
+    },
+    ondragenter: function(event) {
+      const item = event.relatedTarget
+
+      //sprawdzanie type
+      const dropzone = event.currentTarget
+      const itemType = item.getAttribute('type');
+      const dropzoneType = dropzone.getAttribute('type');
+      //
+
+      console.log(itemType, dropzoneType);
+
+      if (itemType !== dropzoneType) return;
+
+      item.classList.remove('cannot-drop')
+      item.classList.add('can-drop')
+    },
+    ondragleave: function(event) {
+      const item = event.relatedTarget
+      item.classList.remove('can-drop')
+      item.classList.add('cannot-drop')
+    }
+  })
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 className="title">Poznajemy warstwy lasu</h1>
+
+      <div className="board" />
+
+      <div className='item' type='1' style={{ left: '100px', top: '100px' }}>I'm an item 1</div>
+      <div className='item' type='2' style={{ left: '100px', top: '250px' }}>I'm an item 2</div>
+      <div className='item' type='3' style={{ left: '100px', top: '400px' }}>I'm an item 3</div>
+
+      <div className="dropzone" type='1' style={{ left: '100px', top: '600px' }}>
+        <h3>Dropzone 1</h3>
+      </div>
+
+      <div className="dropzone" type='2' style={{ left: '300px', top: '600px' }}>
+        <h3>Dropzone 2 </h3>
+      </div>
+
+      <div className="dropzone" type='3' style={{ left: '500px', top: '600px' }}>
+        <h3>Dropzone3</h3>
+      </div>
     </div>
   );
 }
